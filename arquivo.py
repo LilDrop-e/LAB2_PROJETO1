50659d6705bc13d4a85cc9e48dafd0f0af53b33e
LilDrop-e <pxaum21@gmail.com>
2025-08-13 19:40:04 -0300

    daaaammmn second commit

--- arquivo.py
+++ arquivo.py
@@ -2,3 +2,10 @@ def cumprimento(nome):
     return f"Olá, {nome}"
 
 print(cumprimento("Pedro Artur Duran Oliveira"))  
+
+import random
+def numero_aleatorio():
+    numeros = [random.randint(1, 100) for _ in range(7)]
+    return sum(numeros) / len(numeros)
+media = numero_aleatorio()
+print(f"Média dos 7 números aleatórios: {media:.2f}")
